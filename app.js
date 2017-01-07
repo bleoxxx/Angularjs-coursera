fe1dcb13c9984ba9b6c2f7775996c8bd918820da
Bleo <bleo_x@hotmail.com>
2017-01-07 17:08:36 -0500

    6

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 (function () {
 'use strict';
 
-var app = angular.module('lunchChecker', [])
-    app.controller('lunchMessage', function ($scope) {
-       $scope.ButtonClick = function () {
-            $scope.Message = "Button clicked."
-        }
-    });
-
+var VLogin = angular.module('myApp',[]);
+      VLogin.controller('lunchMessage', ['$scope',function($scope) {
+        $scope.clicked = function(){
+    alert("Clicked");
+}
+}]);
 })();

cb429f5790386adf25a8589047ccd66a0f2528ca
Bleo <bleo_x@hotmail.com>
2017-01-07 18:21:34 -0500

    21

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
 var app = angular.module('myApp',[]);
       app.controller('lunchMessage', function ($scope) {
        $scope.ButtonClick = function () {
-            $scope.Message = "Please enter data first , Enjoy! , Too Much!";
+            $scope.message =  function () {
+            return "Please enter data first , Enjoy! , Too Much!";
+            };
       };
      }]);
 })();
